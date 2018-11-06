7e09fbe77b09e799e63db78f2a53702d87bc8f97
David Hadley <d.r.hadley@warwick.ac.uk>
2018-11-06 10:54:56 +0000

    Init project.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "TH1F.h"
+#include "TRandom3.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    TH1F* hist = new TH1F("h", "h", 1000, -5, 5);
+    TRandom3 rando;
+    for(int i=0; i < 1000; ++i) { hist->Fill(rando.Gaus()); }
+    TCanvas* canv = new TCanvas("c");
+    hist->Draw();
+    canv->SaveAs("testplot.pdf");
+    return 0;
+}
